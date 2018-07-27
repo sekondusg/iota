1ae1cd8583c02829670d8b88307d59fe693928a1
Dennis Gilbert <dennis@pdx.edu>
2018-07-26 17:27:05 -0700

    adding client-side update feature

--- iota.py
+++ iota.py
@@ -9,6 +9,7 @@ from AWSIoTPythonSDK.MQTTLib import AWSIoTMQTTShadowClient
 
 from pyfirmata import Arduino, util
 import os
+import signal
 
 
 class Iota:
@@ -17,6 +18,7 @@ class Iota:
     outlet2 = "off"
     motion = "false"
     temperature = 0.0
+    sense = None
 
     thingEndpoint = "a3lybv9v64fkof.iot.us-west-2.amazonaws.com"
     awsDir = "/home/dennis/.aws"
@@ -37,7 +39,6 @@ class Iota:
         self.log.info('init(): retrieving AWS Shadow')
         self.shadow = self.client.createShadowHandlerWithName("iota", True)
         self.log.info('init(): registering delta callback')
-        # self.shadow.shadowRegisterDeltaCallback(onDelta)
         self.log.info('init(): Iota created')
 
         # Setup the Arduino Firmata interface
@@ -120,15 +121,15 @@ class Iota:
                         self.log.info('onDelta() invalid value for delta update to: ' + str(value))
             if len(changes) > 0:
                 self.log.info('onDelta() detected changes to: ' + str(changes))
-                self.shadowUpdate(changes)
+                self.shadowUpdate(reported=changes)
             self.log.info(
                 "onDelta(): responseStatus: " + str(responseStatus) + ", token: " + str(token) + ", payload: " + str(
                     pretty))
         except Exception as ex:
             self.log.info("onDelta() exception: " + str(ex))
 
-    def shadowUpdate(self, changes):
-        self.log.info('iota.shadowUpdate(): starting. preparing to update device shadow for changes: ' + str(changes))
+    def shadowUpdate(self, reported=None, desired=None):
+        self.log.info('iota.shadowUpdate(): starting. preparing to update device shadow for reported: ' + str(reported) + ", or desired: " + str(desired))
         update = {
             'state': {
                 'reported': {},
@@ -136,8 +137,13 @@ class Iota:
             }
         }
 
-        for change in changes:
-            update['state']['reported'][change[0]] = change[1]
+        if reported is not None:
+            for change in reported:
+                update['state']['reported'][change[0]] = change[1]
+
+        if desired is not None:
+            for change in desired:
+                update['state']['desired'][change[0]] = change[1]
 
         doc = json.dumps(update)
         self.log.info('iota.shadowUpdate(): calling shadow.shadowUpdate. srcJSONPayload: ' + doc)
@@ -179,9 +185,21 @@ class Iota:
         else:
             logging.error("setOutlet2: invalid value given for setting outlet2: " + value)
 
+    def getTemp(self):
+        logging.info("getTemp(): getting temperature through Firmata")
+        return( str(round(self.board.analog[1].read() * 500) ))
+
+    def getRH(self):
+        logging.info("getTemp(): getting relative humidity through Firmata")
+        return(str(round(100 * self.board.analog[0].read())))
+
     def getMotion(self):
-        logging.info("getMotion: getting value of motion")
-        return self.motion
+        logging.info("getTemp(): getting motion through Firmata")
+        mot = self.d7.read()
+        if mot == None:
+            return 'na'
+        else:
+            return str(mot)
 
     def setMotion(self, value):
         if value in ['true', 'false']:
@@ -190,8 +208,32 @@ class Iota:
         else:
             logging.error("setMotion: invalid value given for setting motion: " + value)
 
+    def getSense(self):
+        logging.info("getSense(): getting values of all sensors")
+        return( (self.getTemp(), self.getRH(), self.getMotion(),) )
+
     def listen(self):
         while True:
+
+            # sense = (temp, rh, motion)
+            sense = self.getSense()
+            if self.sense is None or sense != self.sense:
+                changes = []
+
+                '''
+                if self.sense[0] != sense[0]:
+                    changes.append( ("temp", sense[0],) )
+                if self.sense[1] != sense[1]:
+                    changes.append( ("rh", sense[1],) )
+                if self.sense[2] != sense[2]:
+                    changes.append( ("motion", sense[2],) )
+                '''
+                changes.append(("temp", sense[0],))
+                changes.append(("rh", sense[1],))
+                changes.append(("motion", sense[2],))
+                self.shadowUpdate(reported=changes)
+            self.sense = sense
+
             time.sleep(1)
 
 
@@ -220,17 +262,54 @@ def onDelta(payload, responseStatus, token):
 if __name__ == '__main__':
     parser = OptionParser()
     parser.add_option("-D", "--debug", dest="debug", action='store_true', default=False, help="Run in debug mode")
+    parser.add_option("-1", "--outlet1", dest="outlet1", action='store', default=None, type="string", help="set outlet 1 to given value")
+    parser.add_option("-2", "--outlet2", dest="outlet2", action='store', default=None, type="string", help="set outlet 2 to given value")
     parser.add_option("-f", "--fore", dest="fore", action='store_true', default=False,
                       help="Run as a foreground process instead of a daemon")
 
     (options, args) = parser.parse_args()
 
+    print("parsed arguments")
+    def shutdown(signum, frame):
+        iota.disconnect()
+        sys.exit(0)
+
     if not options.debug:
-        if options.fore:
-            print('\033]0;IoTaAgent\a')
-            iota = Iota()
-            iota.listen()
-        else:
-            with daemon.DaemonContext():
+        print("not in debug mode")
+        # check for any direct command line commands, otherwise run as a daemon
+        if options.outlet1 is None and options.outlet2 is None:
+            print("in daemon mode")
+            if options.fore:
+                print('\033]0;IoTaAgent\a')
                 iota = Iota()
+                iota.shadow.shadowRegisterDeltaCallback(onDelta)
                 iota.listen()
+            else:
+                print("starting in background daemon mode")
+                with daemon.DaemonContext(
+                    signal_map = {
+                        signal.SIGTERM: shutdown,
+                        signal.SIGTSTP: shutdown
+                    }
+                ):
+                    iota = Iota()
+                    iota.shadow.shadowRegisterDeltaCallback(onDelta)
+                    iota.listen()
+        else:
+            print("starting in non-daemon mode")
+            iota = Iota()
+            changes = []
+            if options.outlet1 is not None:
+                if options.outlet1 in ["on", "off"]:
+                    changes.append(("outlet1", options.outlet1,))
+                else:
+                    print("option outlet1 must be either 'on' or 'off', given: " + options.outlet1)
+            if options.outlet2 is not None:
+                if options.outlet2 in ["on", "off"]:
+                    changes.append( ("outlet2", options.outlet2,) )
+                else:
+                    print("option outlet2 must be either 'on' or 'off', given: " + options.outlet2)
+
+            if len(changes) > 0:
+                iota.shadowUpdate(desired=changes)
+            iota.disconnect()
